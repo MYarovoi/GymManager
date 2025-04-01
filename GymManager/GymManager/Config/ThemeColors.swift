b15f960c99fcd08db615795fa1ac7683e8eb2dab
m.yarovoi <m.yarovoi96@gmail.com>
2025-04-01 17:28:33 +0300

    Project setup

---
+++ GymManager/GymManager/Config/ThemeColors.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColors.swift
+//  GymManager
+//
+//  Created by Никита Яровой on 01.04.2025.
+//
+
+import UIKit
+
+struct ThemeColors {
+    static let darkBlue = UIColor(hexString: "264653")
+    static let coral = UIColor(hexString: "2A9D8F")
+    static let yellow = UIColor(hexString: "E9C46A")
+    static let orange = UIColor(hexString: "F4A261")
+    static let softRed = UIColor(hexString: "E76F51")
+}
---
+++ GymManager/GymManager/GymManagerApp.swift
@@ -0,0 +1,30 @@
+//
+//  GymManagerApp.swift
+//  GymManager
+//
+//  Created by Никита Яровой on 01.04.2025.
+//
+
+import SwiftUI
+import Firebase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct GymManagerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
